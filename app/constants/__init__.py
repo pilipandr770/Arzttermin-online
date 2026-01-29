9084ff186b679599403e4c53ee65eb6471997fe0
pilipandr770 <your-email@example.com>
2026-01-29 21:00:59 +0100

    Fix __init__.py: remove non-existent constant imports

--- app/constants/__init__.py
+++ app/constants/__init__.py
@@ -1,23 +1,11 @@
 """
 Constants package
 """
-from app.constants.specialities import (
-    SPECIALITIES,
-    LANGUAGES,
-    INSURANCE_TYPES,
-    TIMESLOT_DURATION_MINUTES,
-    BOOKING_STATUS,
-    WORKING_HOURS
-)
+from app.constants.specialities import SPECIALITIES
 from app.constants.cities import MAJOR_GERMAN_CITIES, GERMAN_STATES
 
 __all__ = [
     'SPECIALITIES',
-    'LANGUAGES',
-    'INSURANCE_TYPES',
-    'TIMESLOT_DURATION_MINUTES',
-    'BOOKING_STATUS',
-    'WORKING_HOURS',
     'MAJOR_GERMAN_CITIES',
     'GERMAN_STATES'
 ]
